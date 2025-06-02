571d07f00a3818903a074ca89f093250b40060c8
Trane <Trane_67@hotmail.de>
2025-06-02 20:44:54 +0200

    update users

--- users/api/serializers.py
+++ users/api/serializers.py
@@ -15,34 +15,65 @@ class ProfileSerializer(serializers.ModelSerializer):
     username = serializers.CharField(source="user.username", read_only=True)
     email = serializers.EmailField(source="user.email")
     type = serializers.CharField(source="user.type", read_only=True)
-    first_name = serializers.CharField(source="user.first_name", required=False)
+    first_name = serializers.CharField(
+        source="user.first_name", required=False)
     last_name = serializers.CharField(source="user.last_name", required=False)
-    created_at = serializers.DateTimeField(source="user.date_joined", read_only=True)
-    
+    created_at = serializers.DateTimeField(
+        source="user.date_joined", read_only=True)
+
     file = serializers.ImageField(required=False, allow_null=True)
     location = serializers.CharField(required=False, allow_null=True)
     tel = serializers.CharField(required=False, allow_null=True)
     description = serializers.CharField(required=False, allow_null=True)
     working_hours = serializers.IntegerField(required=False)
-    
+
     class Meta:
         model = Profile
-        fields = ['user', 'username', 'first_name', 'last_name', 'file', 'location', 'tel', 'description', 'working_hours', 'type', 'email', 'created_at']
+        fields = ['user', 'username', 'first_name', 'last_name', 'file', 'location',
+                  'tel', 'description', 'working_hours', 'type', 'email', 'created_at']
+
+    def update(self, instance, validated_data):
+        user_data = validated_data.pop("user", {})
+
+        # Wenn z. B. first_name, last_name oder email in user_data drin sind, updaten wir den User
+        if user_data:
+            user = instance.user
+            # Beispiel: user_data könnte {'first_name': 'NeuerVorname', 'last_name': 'NeuerNachname', 'email': 'neue@adresse.de'}
+            first_name = user_data.get("first_name", None)
+            last_name = user_data.get("last_name", None)
+            email = user_data.get("email", None)
+
+            if first_name is not None:
+                user.first_name = first_name
+            if last_name is not None:
+                user.last_name = last_name
+            if email is not None:
+                user.email = email
+
+            user.save()
+
+        # Nun kümmern wir uns um die verbleibenden Profile-Felder
+        # validated_data enthält jetzt nur noch: file, location, tel, description, working_hours (sofern übergeben)
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+
+        instance.save()
+        return instance
 
 
 # class SingleProfileSerializer(serializers.ModelSerializer):
-    
+
 #     username = serializers.CharField(source="user.username", read_only=True)
-    
-   
-#     location = serializers.CharField() 
+
+
+#     location = serializers.CharField()
 #     tel = serializers.IntegerField()
 #     description = serializers.CharField()
 #     working_hours = serializers.IntegerField()
 #     type = serializers.CharField(source="user.type", read_only=True)
 #     email = serializers.EmailField(source="user.email", read_only=True)
 #     created_at = serializers.DateTimeField(source="user.date_joined", read_only=True)
-    
+
 
 #     class Meta:
 #         model = Profile
