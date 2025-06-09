0c1a41ea2a0f6323d39d2411b5c9f9e0d364363f
Trane <Trane_67@hotmail.de>
2025-06-09 15:50:51 +0200

    added reviews

--- reviews/models.py
+++ reviews/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 from django.conf import settings
+from marktplace.models import Offer
 
 # Create your models here.
 class Review(models.Model):
-    product = models.ForeignKey("products.Product", on_delete=models.CASCADE, related_name="reviews")
+    product = models.ForeignKey(Offer, on_delete=models.CASCADE, related_name="reviews")
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="review_user")
     rating = models.PositiveIntegerField()
     comment = models.TextField(blank=True, null=True)
