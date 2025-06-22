a76542d40903857bf1ebb39c399abf3c100267ee
Trane <Trane_67@hotmail.de>
2025-06-22 23:19:27 +0200

    add reviews

--- reviews/api/views.py
+++ reviews/api/views.py
@@ -1,9 +1,17 @@
 from rest_framework import generics
+from rest_framework.response import Response
 from .serializers import ReviewSerializer
 from reviews.models import Review
-from rest_framework.permissions import IsAuthenticatedOrReadOnly, AllowAny    
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, AllowAny
+from rest_framework import status
+from marktplace.models import Offer
+
 
 class ReviewView(generics.ListCreateAPIView):
     queryset = Review.objects.all()
     serializer_class = ReviewSerializer
     permission_classes = [AllowAny]
+
+    def perform_create(self, serializer):
+        reviewer = self.request.user
+        serializer.save(reviewer=reviewer)
--- reviews/models.py
+++ reviews/models.py
@@ -2,10 +2,26 @@ from django.db import models
 from django.conf import settings
 from marktplace.models import Offer
 
+
 # Create your models here.
 class Review(models.Model):
-    product = models.ForeignKey(Offer, on_delete=models.CASCADE, related_name="offer")
-    business_user = models.ForeignKey(settings.AUTH_USER_MODEL,blank=True, null=True, on_delete=models.CASCADE, related_name="review_user", limit_choices_to={'type': 'business'})
+    business_user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        blank=True,
+        null=True,
+        on_delete=models.CASCADE,
+        related_name="review_user",
+        limit_choices_to={"type": "business"},
+    )
     rating = models.PositiveIntegerField()
+    reviewer = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        blank=True,
+        null=True,
+        related_name="reviewer_user",
+        limit_choices_to={"type": "customer"},
+    )
     description = models.TextField(blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
